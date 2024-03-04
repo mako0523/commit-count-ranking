67e933ef5fcd10580a13cb8793440f852e6e1ef0
mako0523 <kurimako.0523@gmail.com>
2024-03-04 23:56:10 +0900

    style: フォーマットを適用

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git pull origin main > /dev/null 2>&1
-git log | grep  Author | cut -d ' ' -f 1,2 | sort | uniq -c | sort -nr
+git pull origin main >/dev/null 2>&1
+git log | grep Author | cut -d ' ' -f 1,2 | sort | uniq -c | sort -nr
